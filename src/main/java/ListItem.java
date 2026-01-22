4345506c4dfd0cb66f61895ddb84ea9685a898ad
tehmiw <tehmingwei1@gmail.com>
2026-01-23 05:10:48 +0800

    Level-2

---
+++ src/main/java/ListItem.java
@@ -0,0 +1,9 @@
+public class ListItem {
+    private String desc;
+    public ListItem(String desc){
+        this.desc = desc;
+    }
+    public String getDesc() {
+        return this.desc;
+    }
+}
--- src/main/java/Listie.java
+++ src/main/java/Listie.java
@@ -1,14 +1,27 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Listie {
     public static void main(String[] args) {
+        ArrayList<ListItem> listitems = new ArrayList<>();
         String logo = "Hello! I'm Listie!" +
                 "\nWhat can I do for you?";
         System.out.println(logo);
         //Solution below inspired by https://www.w3schools.com/java/java_user_input.asp
         while (true){
             String userinput = new Scanner(System.in).nextLine();
-            System.out.println(userinput);
+            switch (userinput) {
+                case "bye":
+                    break;
+                case "list":
+                    for (int i = 0; i < listitems.size(); i++) {
+                        System.out.println(String.format("%d. %s", i + 1, listitems.get(i).getDesc()));
+                    }
+                    break;
+                default:
+                    System.out.println(userinput);
+                    listitems.add(new ListItem(userinput));
+            }
             if (userinput.equals("bye")) {
                 break;
             }
