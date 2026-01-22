c6dcc390d4a5f4982ffd60b1540728392529e095
tehmiw <tehmingwei1@gmail.com>
2026-01-23 01:54:29 +0800

    level 0

---
+++ src/main/java/Listie.java
@@ -0,0 +1,8 @@
+public class Listie {
+    public static void main(String[] args) {
+        String logo = "Hello! I'm Listie!" +
+                "\nWhat can I do for you?" +
+                "\nBye. Hope to see you again soon!";
+        System.out.println("Hello from\n" + logo);
+    }
+}
