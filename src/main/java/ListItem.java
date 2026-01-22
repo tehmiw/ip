d9cd38f705dcda3fa29a88fffac8eff1c7a572c9
tehmiw <tehmingwei1@gmail.com>
2026-01-23 05:42:13 +0800

    Level-3

--- src/main/java/ListItem.java
+++ src/main/java/ListItem.java
@@ -1,9 +1,25 @@
 public class ListItem {
     private String desc;
+    private boolean done;
     public ListItem(String desc){
         this.desc = desc;
+        this.done = false;
     }
     public String getDesc() {
         return this.desc;
+    }
+    public void mark() {
+        this.done = true;
+    }
+    public void unmark() {
+        this.done = false;
+    }
+    public boolean isDone() {
+        return this.done;
+    }
+    public String getLine() {
+        return String.format("[%s] %s",
+                this.isDone() ? "X" : " ",
+                this.getDesc());
     }
 }
--- src/main/java/Listie.java
+++ src/main/java/Listie.java
@@ -10,12 +10,32 @@ public class Listie {
         //Solution below inspired by https://www.w3schools.com/java/java_user_input.asp
         while (true){
             String userinput = new Scanner(System.in).nextLine();
-            switch (userinput) {
+            String firstword = userinput.split(" ")[0];
+            switch (firstword) {
+                case "mark":
+                    int listindexm = Integer.parseInt(userinput.split(" ")[1]) - 1;
+                    listitems.get(listindexm).mark();
+                    System.out.println("Nice! I've marked this task as done:\n"
+                        + String.format("%d. %s",
+                            listindexm,
+                            listitems.get(listindexm).getLine()));
+                    break;
+                case "unmark":
+                    int listindexu = Integer.parseInt(userinput.split(" ")[1]) - 1;
+                    listitems.get(listindexu).unmark();
+                    System.out.println("OK, I've marked this task as not done yet:\n"
+                            + String.format("%d. %s",
+                            listindexu,
+                            listitems.get(listindexu).getLine()));
+                    break;
                 case "bye":
                     break;
                 case "list":
                     for (int i = 0; i < listitems.size(); i++) {
-                        System.out.println(String.format("%d. %s", i + 1, listitems.get(i).getDesc()));
+
+                        System.out.println(String.format("%d. %s",
+                                i + 1,
+                                listitems.get(i).getLine()));
                     }
                     break;
                 default:
