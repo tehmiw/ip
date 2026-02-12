5cefd6ba0b37565fc8122375a33af523fab1d395
tehmiw <tehmingwei1@gmail.com>
2026-02-13 04:26:33 +0800

    Add A-Assertions
    There are parts in the code where calling a method should return
    something that should not be null, or change something of which we can
    check the status of.
    
    By making use of assert, we are able to check if there are any errors
    made by the programmer that cause buggy behaviour at certain lines that
    act as 'breakpoints'. In this case, those assert statements were used.

--- src/main/java/listiepackage/Listie.java
+++ src/main/java/listiepackage/Listie.java
@@ -13,6 +13,7 @@ public class Listie {
         Ui ui = new Ui();
         try {
             listItems = storage.loadItems();
+            assert listItems != null : "Loaded TaskList should not be null";
         } catch (FileNotFoundException e) {
 
         }
@@ -21,19 +22,23 @@ public class Listie {
             try {
                 String userInput = ui.readCommand();
                 String firstWord = Parser.getCommand(userInput);
+                assert firstWord != null : "Parser.getCommand should not return null";
                 switch (firstWord) {
                 case "todo":
                     ToDo itemAddedT = new ToDo(Parser.getTodoDescription(userInput));
+                    assert itemAddedT != null : "Created ToDo should not be null";
                     listItems.add(itemAddedT);
                     ui.showTaskAdded(itemAddedT, listItems.size());
                     break;
                 case "deadline":
                     Deadline itemAddedD = Parser.parseDeadline(userInput);
+                    assert itemAddedD != null : "Created Deadline should not be null";
                     listItems.add(itemAddedD);
                     ui.showTaskAdded(itemAddedD, listItems.size());
                     break;
                 case "event":
                     Event itemAddedE = Parser.parseEvent(userInput);
+                    assert itemAddedE != null : "Created Event should not be null";
                     listItems.add(itemAddedE);
                     ui.showTaskAdded(itemAddedE, listItems.size());
                     break;
@@ -44,12 +49,18 @@ public class Listie {
                     break;
                 case "mark":
                     int markIndex = Parser.parseIndex(userInput);
+                    assert markIndex >= 0 : "Index should be non-negative";
+                    assert markIndex < listItems.size() : "Index should be within bounds";
                     listItems.mark(markIndex);
+                    assert listItems.get(markIndex).isDone() : "Task should be marked";
                     ui.showTaskMarked(listItems.get(markIndex));
                     break;
                 case "unmark":
                     int unmarkIndex = Parser.parseIndex(userInput);
+                    assert unmarkIndex >= 0 : "Index should be non-negative";
+                    assert unmarkIndex < listItems.size() : "Index should be within bounds";
                     listItems.unmark(unmarkIndex);
+                    assert !listItems.get(unmarkIndex).isDone() : "Task should be unmarked";
                     ui.showTaskUnmarked(listItems.get(unmarkIndex));
                     break;
                 case "bye":
